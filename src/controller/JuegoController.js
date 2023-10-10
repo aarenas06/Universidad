8b50a3deb77b3454219685bb0d3e04c9383ead20
Diego Arenas <diego_arenasmo@fet.edu.co>
2023-10-10 17:44:47 -0500

    Se crean las Peticiones

--- src/controller/JuegoController.js
+++ src/controller/JuegoController.js
@@ -1,13 +1,36 @@
 import { getConnection } from "./../database/database";
 
-
 const getPlayer = async (req, res) => {
+  const connection = await getConnection();
+  const result = await connection.query("select * from players");
+  console.log(result);
+  res.json(result);
+};
+
+const getApectos = async (req, res) => {
   const connection = await getConnection();
   const result = await connection.query("select * from aspectos");
   console.log(result);
   res.json(result);
 };
 
+const getPreguntas = async (req, res) => {
+  const connection = await getConnection();
+  const result = await connection.query("select * from preguntas");
+  console.log(result);
+  res.json(result);
+};
+
+const getAsignaciones = async (req, res) => {
+  const connection = await getConnection();
+  const result = await connection.query("select * from asignaciones");
+  console.log(result);
+  res.json(result);
+};
+
 export const methods = {
   getPlayer,
+  getApectos,
+  getPreguntas,
+  getAsignaciones,
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,5 +2,8 @@ import { Router } from "express";
 import { methods as JuegoController } from "../controller/JuegoController";
 const router = Router();
 
-router.get("/", JuegoController.getPlayer);
+router.get("/Players", JuegoController.getPlayer);
+router.get("/Aspectos", JuegoController.getPlayer);
+router.get("/Preguntas", JuegoController.getPlayer);
+router.get("/Asignaciones", JuegoController.getPlayer);
 export default router;
